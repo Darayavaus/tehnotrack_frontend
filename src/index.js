9bc257ba5917513806a652c647812bdcad2cfcc8
Darayavaus <bor.astat@gmail.com>
2018-10-01 20:38:15 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// require("./lib/test.js"); // AMD
+
+import say from "./lib/test.js" //ES-module
+// say("fffffff")
+// document.getElementById("test1").style.display = "block";
+document.getElementById('test1').innerHTML = say("Javascript");
